9390dea7fc16b3d0422891029f4c96e27036d329
Anton Mitsengendler <anton.mitsengendler@outlook.com>
2026-03-31 18:31:56 +0000

    prototype of comparing with index

--- src/utils/Diff.ts
+++ src/utils/Diff.ts
@@ -3,22 +3,30 @@ const daff = require("daff");
 export const diff = (left: [][], right: [][], ref: React.RefObject<any>) => {
   var instance = ref.current.hotInstance;
   var result = [];
-  let tableLeft = new daff.TableView(left);
-  let tableRight = new daff.TableView(right);
+  const leftNorm = normalizeTable(left, 2);
+  const rightNorm = normalizeTable(right, 2);
+
+  let tableLeft = new daff.TableView(leftNorm);
+  let tableRight = new daff.TableView(rightNorm);
 
   tableLeft.trim();
   tableRight.trim();
 
-  var ct = daff.compareTables(tableLeft, tableRight);
-
-  let align = ct.align();
-  let output = new daff.TableView([]);
   let flags = new daff.CompareFlags();
   flags.show_unchanged = false;
   flags.always_show_header = true;
   flags.always_show_order = true;
   flags.never_show_order = false;
   flags.unchanged_context = true;
+
+  flags.addPrimaryKey(leftNorm[0][1]);
+  flags.addPrimaryKey(leftNorm[0][4]);
+
+  var ct = daff.compareTables(tableLeft, tableRight, flags);
+
+  let align = ct.align();
+  let output = new daff.TableView([]);
+  
 
   var td = new daff.TableDiff(align, flags);
   td.hilite(output);
@@ -28,3 +36,10 @@ export const diff = (left: [][], right: [][], ref: React.RefObject<any>) => {
     instance.loadData(result);
   }
 };
+
+function normalizeTable(data: any[][], headerRowIndex: number) {
+  const header = data[headerRowIndex];
+  const body = data.slice(headerRowIndex + 1);
+  return [header, ...body];
+}
+
